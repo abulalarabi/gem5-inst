7b943eade3c10bb8ca033ec98bfa75db50d125a6
Al Arabi <arabiabulal@gmail.com>
2023-04-19 14:16:20 -0500

    initial

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
+echo "Installation script V1 by arabi"
+sleep 1
+echo "Updating system"
+sleep 1
+sudo apt update -y
+echo "Upgrading system"
+sleep 1
+sudo apt upgrade -y
+sleep 1
+echo "Installing dependencies"
+sleep 1
 sudo apt install build-essential git m4 scons zlib1g zlib1g-dev \
     libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev \
     python3-dev libboost-all-dev pkg-config
-
+echo "Cloning GEM5 repo"
+sleep 1
 git clone https://gem5.googlesource.com/public/gem5
+echo "Navigating to GEM5 folder"
+sleep 1
 cd gem5
+echo "Building GEM5 for ARM with scons"
+sleep 1
 scons build/ARM/gem5.opt -j 4
